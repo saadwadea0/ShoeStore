931b1be452757ca7b0a20c5fd2f6fee9c5a27a3d
Saad Wade <saadwadea0@gmail.com>
2022-08-19 13:37:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/shoestore/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.example.shoestore
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LoginFragment : Fragment() {
+
+
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/shoestore/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.shoestore
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
